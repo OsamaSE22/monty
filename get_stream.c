59a49524a93e9e2d7eae2e3289f890fa44a4c98f
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-11 22:03:42 +0300

    First_Version

--- get_stream.c
+++ get_stream.c
@@ -8,7 +8,7 @@ void getting_stream_failed(char *fileName)
 {
 	fprintf(stderr, "Error: Can't open file %s\n", fileName);
 	free_arguments();
-	fclose(arguments->stream);
+	/*fclose(arguments->stream);*/
 	exit(EXIT_FAILURE);
 }
 
